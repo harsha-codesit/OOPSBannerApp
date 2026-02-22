0c6a61fd162aeb32159b5cf22384a5fc9f372a7f
harsha-codesit <harshadha2018@gmail.com>
2026-02-22 20:38:45 +0530

    Added UC3 printing oops banner using string.join

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,11 +1,39 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("   **  "  + "    **  " + " ******" + "   ******");
-        System.out.println("**    **" + "**    **" + "**    **"+ " **");
-        System.out.println("**    **" + "**    **" + "**    **"+ "  **");
-        System.out.println("**    **" + "**    **" + "******"  + "    ***");
-        System.out.println("**    **" + "**    **" + "**"      + "        **");
-        System.out.println("**    **" + "**    **" + "**"      + "    **");
-        System.out.println("   **  "  + "    **  " +  "**"     + " ***");
+
+        // O
+        System.out.println(String.join(" ", " ", "*", "*", "*", "*", "*", " "));
+        System.out.println(String.join(" ", "*", " ", " ", " ", " ", "*"));
+        System.out.println(String.join(" ", "*", " ", " ", " ", " ", "*"));
+        System.out.println(String.join(" ", "*", " ", " ", " ", " ", "*"));
+        System.out.println(String.join(" ", " ", "*", "*", "*", "*", "*", " "));
+
+        System.out.println();
+
+        // O
+        System.out.println(String.join(" ", " ", "*", "*", "*", "*", "*", " "));
+        System.out.println(String.join(" ", "*", " ", " ", " ", " ", "*"));
+        System.out.println(String.join(" ", "*", " ", " ", " ", " ", "*"));
+        System.out.println(String.join(" ", "*", " ", " ", " ", " ", "*"));
+        System.out.println(String.join(" ", " ", "*", "*", "*", "*", "*", " "));
+
+        System.out.println();
+
+        // P
+        System.out.println(String.join(" ", "*", "*", "*", "*", "*"));
+        System.out.println(String.join(" ", "*", " ", " ", " ", "*"));
+        System.out.println(String.join(" ", "*", "*", "*", "*", "*"));
+        System.out.println(String.join(" ", "*"));
+        System.out.println(String.join(" ", "*"));
+
+        System.out.println();
+
+        // S
+        System.out.println(String.join(" ", " ", "*", "*", "*", "*", "*"));
+        System.out.println(String.join(" ", "*"));
+        System.out.println(String.join(" ", " ", "*", "*", "*", "*"));
+        System.out.println(String.join(" ", " ", " ", " ", " ", "*"));
+        System.out.println(String.join(" ", "*", "*", "*", "*", "*"));
     }
 }
