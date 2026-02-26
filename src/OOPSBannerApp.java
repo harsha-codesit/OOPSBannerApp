6ec655d0d537f403be21e13fef43710a6b57efb5
harsha-codesit <harshadha2018@gmail.com>
2026-02-26 12:12:53 +0530

    updated UC6

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -2,20 +2,64 @@ public class OOPSBannerApp{
 
     public static void main(String[] args) {
 
-        // Inline declaration + initialization using String.join()
-        String[] banner = {
-                String.join("", " *****  ", " *****  ", " ****** ", " *****  "),
-                String.join("", "*     * ", "*     * ", "*     * ", "*     * "),
-                String.join("", "*     * ", "*     * ", "*     * ", "*       "),
-                String.join("", "*     * ", "*     * ", " ****** ", " *****  "),
-                String.join("", "*     * ", "*     * ", "*       ", "      * "),
-                String.join("", "*     * ", "*     * ", "*       ", "*     * "),
-                String.join("", " *****  ", " *****  ", "*       ", " *****  ")
-        };
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        // Enhanced for loop
+        // Create banner array
+        String[] banner = new String[7];
+
+        // Build banner line by line
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("  ",
+                    oPattern[i],
+                    oPattern[i],
+                    pPattern[i],
+                    sPattern[i]);
+        }
+
+        // Print using enhanced for loop
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Method to generate O pattern
+    public static String[] getOPattern() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    // Method to generate P pattern
+    public static String[] getPPattern() {
+        return new String[]{
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+    }
+
+    // Method to generate S pattern
+    public static String[] getSPattern() {
+        return new String[]{
+                "  *****  ",
+                " *       ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                "       * ",
+                "  *****  "
+        };
     }
 }
