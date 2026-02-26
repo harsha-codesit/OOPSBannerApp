9e998690befd9e36b9185b4859e54f57bdf8a718
harsha-codesit <harshadha2018@gmail.com>
2026-02-26 12:19:11 +0530

    Update OOPSBannerApp.java

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -6,10 +6,9 @@ public class OOPSBannerApp{
         String[] pPattern = getPPattern();
         String[] sPattern = getSPattern();
 
-        // Create banner array
         String[] banner = new String[7];
 
-        // Build banner line by line
+        
         for (int i = 0; i < 7; i++) {
             banner[i] = String.join("  ",
                     oPattern[i],
@@ -18,13 +17,13 @@ public class OOPSBannerApp{
                     sPattern[i]);
         }
 
-        // Print using enhanced for loop
+        
         for (String line : banner) {
             System.out.println(line);
         }
     }
 
-    // Method to generate O pattern
+    
     public static String[] getOPattern() {
         return new String[]{
                 "  *****  ",
@@ -37,7 +36,7 @@ public class OOPSBannerApp{
         };
     }
 
-    // Method to generate P pattern
+    
     public static String[] getPPattern() {
         return new String[]{
                 " ******  ",
@@ -50,7 +49,7 @@ public class OOPSBannerApp{
         };
     }
 
-    // Method to generate S pattern
+    
     public static String[] getSPattern() {
         return new String[]{
                 "  *****  ",
